3edef325dbac469390dad507d27a8e3cfcb36095
FlaminTorch14 <jag200312@gmail.com>
2022-04-07 14:26:47 -0600

    player foundations

--- DungeonGame/DungeonGame.java
+++ DungeonGame/DungeonGame.java
@@ -2,7 +2,6 @@ import javax.sql.StatementEvent;
 
 public class DungeonGame {
     public static void main(String[] args) {
-        Guard dave = new Guard(25, 4, 3);
-        System.out.printf("damage is %d%n", dave.attack());
+        
     }
 }
