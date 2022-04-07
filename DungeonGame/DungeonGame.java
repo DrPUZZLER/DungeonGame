f05819978c70b043347d02b22495c9d4bcd59359
FlaminTorch14 <jag200312@gmail.com>
2022-04-07 14:37:06 -0600

    Update DungeonGame.java

--- DungeonGame/DungeonGame.java
+++ DungeonGame/DungeonGame.java
@@ -2,6 +2,7 @@ import javax.sql.StatementEvent;
 
 public class DungeonGame {
     public static void main(String[] args) {
-        
+        Player ryan = new Player(-11, -1,-3, -10);
+        System.out.print(ryan.swipe());
     }
 }
