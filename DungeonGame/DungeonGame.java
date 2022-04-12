acca21c9b94eff15f4a283bd5e884d1a79e66aa4
Ryan Zollinger <ryanzolly@gmail.com>
2022-04-12 14:47:52 -0600

    Update DungeonGame.java

--- DungeonGame/DungeonGame.java
+++ DungeonGame/DungeonGame.java
@@ -1,6 +1,5 @@
 public class DungeonGame {
     public static void main(String[] args) {
-        Player ryan = new Player(-11, -1,-3, -10);
-        System.out.print(ryan.swipe());
+        
     }
 }
