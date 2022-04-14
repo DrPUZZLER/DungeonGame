8536f5277e684c3bf5d9e722c4ca3eda48b299c7
Jonathan C <102754994+J0NATHANC@users.noreply.github.com>
2022-04-14 10:01:12 -0600

    riddle logic

--- DungeonGame/DungeonGame.java
+++ DungeonGame/DungeonGame.java
@@ -1,8 +1,7 @@
-import javax.sql.StatementEvent;
-
 public class DungeonGame {
     public static void main(String[] args) {
-        Guard dave = new Guard(25, 4, 3);
-        System.out.printf("damage is %d%n", dave.attack());
+       
+        Riddle.key(); 
+        
     }
 }
--- DungeonGame/Riddle.java
+++ DungeonGame/Riddle.java
@@ -1,3 +1,24 @@
+
 public class Riddle {
+
+    public static boolean key = false; 
+
+    public static void askRiddle(){
+
+        System.out.println("I'm tall when I am young, and I'm short when I'm old. ");
+        System.out.println("What am I?");
     
+    }
+    public static void hint(){
+
+            System.out.println("I am a non-living object...");
+        }
+    public static void anotherHint(){
+            System.out.println("I can create light...");
+        }
+    public static void key(){
+        System.out.println("You have recieved a key!");
+        key = true; 
+    }
+
 }
