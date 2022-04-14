aaddb14b77467facc00e6f0faf619448e2ae2078
Ryan Zollinger <ryanzolly@gmail.com>
2022-04-14 13:57:59 -0600

    added classes

---
+++ DungeonGame/Navigation.java
@@ -0,0 +1,23 @@
+public class Navigation {
+    public static void hint1() {
+        
+    }
+    public static void hint2() {
+        
+    }
+    public static void riddle() {
+        
+    }
+    public static void door() {
+        
+    }
+    public static void hallway() {
+        
+    }
+    public static void innerChamber() {
+        
+    }
+    public static void monster() {
+        
+    }
+}
