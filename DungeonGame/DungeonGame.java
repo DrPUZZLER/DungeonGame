70a457149b277a3b1e119fa6190a72c00a18c2f6
Jonathan C <102754994+J0NATHANC@users.noreply.github.com>
2022-04-14 18:18:48 -0600

    Updated (added a Switch Statement)

--- DungeonGame/DungeonGame.java
+++ DungeonGame/DungeonGame.java
@@ -1,6 +1,25 @@
+import java.util.Scanner; 
+
 public class DungeonGame {
     public static void main(String[] args) {
        
+        String userAnswer; 
+        Scanner input = new Scanner(System.in);      
+        
+        do{
+            userAnswer  = input.nextLine(); 
+            switch (userAnswer) {
+                case 0:
+                    System.out.println("Candle");
+                    break;
+                case 1:
+                    System.out.println("candle");
+                    break;
+                default: 
+                    System.out.println("Try Agian...");
 
+            }
+
+        }
     }
 }
